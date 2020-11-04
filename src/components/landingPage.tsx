48a88e3f0aafdffb1c98a0acdc56ade3251d2c54
tyannarae <tyannarae1994@gmail.com>
2020-11-04 13:54:16 -0600

    added a cover image. doesnt look good though.

---
+++ src/components/landingPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../landingPageImg.jpg";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div>The Grass is Greener</div>
+      <img src={logo} className="App-logo" alt="logo" />
+      {/* <img src="../src/landingPageImg.jpg" alt="" /> */}
+    </div>
+  );
+};
+
+export default LandingPage;
