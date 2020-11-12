f28251b43a1bd915577065a52863eb12c9c2b2d5
tyannarae <tyannarae1994@gmail.com>
2020-11-12 14:08:13 -0600

    contents now fit within the plant card

--- src/components/shared/plantCard.tsx
+++ src/components/shared/plantCard.tsx
@@ -5,14 +5,19 @@ import "../../styles/components/plantCard.scss";
 const PlantCard = () => {
   return (
     <div className="plantCardContainer">
-      <span className="plantCommonName">img name</span>
-      <figure className="plantCardDetails">
+      <div className="plantCommonName">img name</div>
+      <div className="plantCardDetails">
         <button className="expand">+</button>
         <img src={testImg} className="plantImage" alt="testing" />
-      </figure>
-      <span className="light tag">light</span>
-      <span className="difficultyLevel tag">difficulty</span>
-      <span className="humidity tag">humidity</span>
+      </div>
+      <div className="tags">
+        <div className="light tag">light</div>
+        <div className="difficultyLevel tag">difficulty</div>
+        <div className="humidity tag">humidity</div>
+        <div className="light tag">light</div>
+        <div className="difficultyLevel tag">difficulty</div>
+        <div className="humidity tag">humidity</div>
+      </div>
     </div>
   );
 };
