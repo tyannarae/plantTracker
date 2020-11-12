441385791d6b9af6e87167f4a52457a974c0ed09
tyannarae <tyannarae1994@gmail.com>
2020-11-12 15:07:14 -0600

    hover effects added

--- src/components/shared/plantCard.tsx
+++ src/components/shared/plantCard.tsx
@@ -5,15 +5,12 @@ import "../../styles/components/plantCard.scss";
 const PlantCard = () => {
   return (
     <div className="plantCardContainer">
-      <div className="plantCommonName">img name</div>
       <div className="plantCardDetails">
         <button className="expand">+</button>
         <img src={testImg} className="plantImage" alt="testing" />
       </div>
+      <div className="plantCommonName">img name</div>
       <div className="tags">
-        <div className="light tag">light</div>
-        <div className="difficultyLevel tag">difficulty</div>
-        <div className="humidity tag">humidity</div>
         <div className="light tag">light</div>
         <div className="difficultyLevel tag">difficulty</div>
         <div className="humidity tag">humidity</div>
