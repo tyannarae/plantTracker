dbb0336dceb16d086a19d16a1422ba387c256280
tyannarae <tyannarae1994@gmail.com>
2020-11-29 16:44:13 -0600

    created selected plant context and are able to pass the plant to the modal

--- src/context/pages/searchPage.ts
+++ src/context/pages/searchPage.ts
@@ -2,11 +2,19 @@ import { createContext, useContext } from 'react'
 import {Plant, Plants} from '../../database/plants'
 
 export type SearchPageContextType = {
+    selectedPlant?: Plant,
+    setSelectedPlant: (chosenPlant: Plant) => void,
+    isModalOpen: boolean,
+    setModalOpen: (isOpen: boolean) => void,
     searchResults: Array<Plant>,
     setSearchResults: (results: Array<Plant>) => void
 }
 
 export const SearchPageContext = createContext<SearchPageContextType>({
+    selectedPlant: undefined,
+    setSelectedPlant: chosenPlant => console.warn('no setSelectedPlant context provider'),
+    isModalOpen: false,
+    setModalOpen: isOpen => console.warn('modal no context provider'),
     searchResults: Plants,
     setSearchResults: results => console.warn('no search context provider')
 })
