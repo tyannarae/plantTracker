0db94743ec0032da032afb132a03f58aa368b18d
tyannarae <tyannarae1994@gmail.com>
2020-11-30 11:16:17 -0600

    user can now exit the expanded modal

--- src/pages/searchPage.tsx
+++ src/pages/searchPage.tsx
@@ -10,13 +10,20 @@ const SearchPage: FunctionComponent = () => {
   const [isModalOpen, setModalOpen] = useState(false)
   const [searchResults, setSearchResults] = useState<Array<Plant>>(Plants)
   const [selectedPlant, setSelectedPlant] = useState<Plant | undefined >(undefined)
+
+  const closeModal = () =>{
+    setModalOpen(!isModalOpen)
+  }
   return (
     <SearchPageContext.Provider value={{searchResults, setSearchResults, isModalOpen, setModalOpen, selectedPlant, setSelectedPlant}}>
     <div className="landingPageContainer">
         <Modal
           isOpen={isModalOpen}
           contentLabel="Example Modal"
-        > {selectedPlant?.commonName[0]} </Modal>
+        > {selectedPlant?.commonName[0]}
+        <button onClick={closeModal}>
+          X
+          </button> </Modal>
       <NavBar />
       <div className="resultsContainer">
         <div className="featuredPlantsHeader">Featured Plants</div>
