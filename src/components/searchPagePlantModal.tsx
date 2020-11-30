daf72c2b79946bc49d2fe6e4fc6bf33d150ad9db
tyannarae <tyannarae1994@gmail.com>
2020-11-30 16:36:30 -0600

    mapped over our light requirements array

--- src/components/searchPagePlantModal.tsx
+++ src/components/searchPagePlantModal.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from "react";
 import { useSearchContext } from '../context/pages/searchPage'
+import '../styles/components/searchPagePlantModal.scss'
 
 const SearchPagePlantModal: FunctionComponent = () => {
     const {selectedPlant }= useSearchContext();
@@ -7,13 +8,41 @@ const SearchPagePlantModal: FunctionComponent = () => {
   const closeModal = () =>{
     setModalOpen(!isModalOpen)
   }
+
+  const getLightRequirements =() =>{
+      
+      if (selectedPlant?.lightRequirements === undefined) {
+         return undefined
+      } else {
+        
+          return(
+            <div>Light Requirements: 
+                        {selectedPlant?.lightRequirements.map((lightReq) => (
+                    <div>{lightReq}</div>
+          
+          ))}
+            </div> 
+          )        
+        }
+  
+  }
     return (
        
-            <div>
-                  <button onClick={closeModal}> X </button>
+            <div className={'searchPageModalContainer'}>
+                <button className={'exitButton'}onClick={closeModal}> X </button>
                 <div>{selectedPlant?.commonName[0]}</div>
                 <img src={selectedPlant?.img} alt=''></img>
-                <div>{selectedPlant?.id}</div>
+                <div> Scientific Name: {selectedPlant?.scientificName}</div>
+                <div> Difficulty: {selectedPlant?.difficultyLevel}</div>
+                <div> Humidity Level: {selectedPlant?.humidityLevel}</div>
+          
+                {getLightRequirements()}
+        
+                
+                <div>Min Temp: {selectedPlant?.minTemp}</div>
+                <div>Max Temp: {selectedPlant?.maxTemp}</div>
+                <div>Misting Requirement: {selectedPlant?.misting}</div>
+
             </div>
 
     )
