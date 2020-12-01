8e7e9cc2102f75ae75cc3dff2080f83b7072302e
tyannarae <tyannarae1994@gmail.com>
2020-12-01 14:46:18 -0600

    made code review changes

--- src/pages/searchPage.tsx
+++ src/pages/searchPage.tsx
@@ -1,41 +1,46 @@
 import React, { FunctionComponent, useState } from "react";
-import Modal from 'react-modal';
-import SearchPagePlantModal from '../components/searchPagePlantModal'
+import Modal from "react-modal";
+import PlantInspector from "../components/plantInspector";
 import NavBar from "../components/navBar";
 import PlantCard from "../components/plantCard";
-import { SearchPageContext } from '../context/pages/searchPage'
+import { SearchPageContext } from "../context/pages/searchPage";
 import { Plants, Plant } from "../database/plants";
 import "../styles/pages/searchPage.scss";
 
 const SearchPage: FunctionComponent = () => {
-  const [isModalOpen, setModalOpen] = useState(false)
-  const [searchResults, setSearchResults] = useState<Array<Plant>>(Plants)
-  const [selectedPlant, setSelectedPlant] = useState<Plant | undefined >(undefined)
+	const [isModalOpen, setModalOpen] = useState(false);
+	const [searchResults, setSearchResults] = useState<Array<Plant>>(Plants);
+	const [selectedPlant, setSelectedPlant] = useState<Plant | undefined>(
+		undefined
+	);
 
-
-  return (
-    <SearchPageContext.Provider value={{searchResults, setSearchResults, isModalOpen, setModalOpen, selectedPlant, setSelectedPlant}}>
-    <div className="landingPageContainer">
-        <Modal
-          isOpen={isModalOpen}
-          contentLabel="Search Page Plant Modal"
-        > 
-          <SearchPagePlantModal/>
-        </Modal>
-      <NavBar />
-      <div className="resultsContainer">
-        <div className="featuredPlantsHeader">
-          Featured Plants
-        </div>
-      </div>
-      <div className="cardsContainer">
-        {searchResults.map((plant) => (
-          <PlantCard key={plant.id} {...plant} />
-        ))}
-      </div>
-    </div>
-    </SearchPageContext.Provider>
-  );
+	return (
+		<SearchPageContext.Provider
+			value={{
+				searchResults,
+				setSearchResults,
+				isModalOpen,
+				setModalOpen,
+				selectedPlant,
+				setSelectedPlant,
+			}}
+		>
+			<div className="landingPageContainer">
+				<Modal isOpen={isModalOpen} contentLabel="Search Page Plant Modal">
+					<PlantInspector />
+				</Modal>
+				<NavBar />
+				<div className="resultsContainer">
+					<div className="featuredPlantsHeader">Featured Plants</div>
+				</div>
+				<div className="cardsContainer">
+					{searchResults.map((plant) => (
+						<PlantCard key={plant.id} {...plant} />
+					))}
+				</div>
+			</div>
+		</SearchPageContext.Provider>
+	);
 };
 
 export default SearchPage;
