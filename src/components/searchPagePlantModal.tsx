ffeb82dfd6a5ac242e245351f8641766a40d15ec
tyannarae <tyannarae1994@gmail.com>
2020-12-01 14:31:06 -0600

    used css grid to seperate information in component.

--- src/components/searchPagePlantModal.tsx
+++ src/components/searchPagePlantModal.tsx
@@ -1,49 +1,73 @@
 import React, { FunctionComponent } from "react";
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
-import { useSearchContext } from '../context/pages/searchPage'
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import { useSearchContext } from "../context/pages/searchPage";
 import { DirectionFacing } from "../models/directionFacing";
-import '../styles/components/searchPagePlantModal.scss'
+import "../styles/components/searchPagePlantModal.scss";
 
 const SearchPagePlantModal: FunctionComponent = () => {
-const {selectedPlant }= useSearchContext();
-const {isModalOpen, setModalOpen} = useSearchContext();
-const closeModal = () =>{
-    setModalOpen(!isModalOpen)
-}
+  const { selectedPlant } = useSearchContext();
+  const { isModalOpen, setModalOpen } = useSearchContext();
+  const closeModal = () => {
+    setModalOpen(!isModalOpen);
+  };
+  const options: Array<string> = [];
 
-const options: Array<string> = [];
+  const getLightRequirements = () => {
+    if (selectedPlant?.lightRequirements === undefined) {
+      return undefined;
+    } else {
+      return (
+        <div>
+          Light Requirements:
+          {selectedPlant?.lightRequirements.map((lightReq) => (
+            <div>{lightReq}</div>
+          ))}
+        </div>
+      );
+    }
+  };
 
-  Object.keys(DirectionFacing).map((direction) => (
-    options.push(direction)
-    ));   
+  Object.keys(DirectionFacing).map((direction) => options.push(direction));
 
+  const addToCollection = () => {
+    console.log("");
+  };
 
-  const getLightRequirements =() =>{
-      if (selectedPlant?.lightRequirements === undefined) {
-         return undefined
-      }
-       else {
-          return(
-            <div>Light Requirements: 
-                {selectedPlant?.lightRequirements.map((lightReq) => (
-                    <div>{lightReq}</div>
-                ))}
-            </div> 
-          )        
-        }
-  }
-const addToCollection = () =>{
-  console.log('');
-}
-
-    return (
-      <div className={"searchPageModalContainer"}>
-        <button className={"exitButton"} onClick={closeModal}>
-          X
-        </button>
+  return (
+    <div className={"searchPageModalContainer"}>
+      <button className={"exitButton"} onClick={closeModal}>
+        X
+      </button>
+      <div className="itemNameAndImgContainer">
         <div>{selectedPlant?.commonName[0]}</div>
-        <img className="searchPageModalImg"src={selectedPlant?.img} alt=""></img>
+        <img
+          className="searchPageModalImg"
+          src={selectedPlant?.img}
+          alt=""
+        ></img>
+      </div>
+      <div className="itemAddToCollectionContainer">
+        Add {selectedPlant?.commonName[0]} to your collection:
+        <div>
+          what direction does this plant face?
+          <Dropdown options={options} placeholder="Select an option"></Dropdown>
+        </div>
+        <div>
+          Is it located in a window seal?
+          <input type="checkbox" />
+        </div>
+        <div>
+          Is it located under a grow light?
+          <input type="checkbox" />
+        </div>
+        <div>
+          When was the last water date?
+          <input type="checkbox" />
+        </div>
+        <button onClick={addToCollection}> Add</button>
+      </div>
+      <div className="itemPlantDetailsContainer">
         <div> Scientific Name: {selectedPlant?.scientificName}</div>
         <div> Difficulty: {selectedPlant?.difficultyLevel}</div>
         <div> Humidity Level: {selectedPlant?.humidityLevel}</div>
@@ -51,30 +75,8 @@ const addToCollection = () =>{
         <div>Min Temp: {selectedPlant?.minTemp}</div>
         <div>Max Temp: {selectedPlant?.maxTemp}</div>
         <div>Misting Requirement: {selectedPlant?.misting}</div>
-        <div className="AddToCollectionContainer">
-          Add {selectedPlant?.commonName[0]} to your collection:
-          <div>
-            what direction does this plant face?
-            <Dropdown
-              options={options}
-              placeholder="Select an option"
-            ></Dropdown>
-          </div>
-          <div>
-            Is it located in a window seal?
-            <input type="checkbox" />
-          </div>
-          <div>
-            Is it located under a grow light?
-            <input type="checkbox" />
-          </div>
-          <div>
-            When was the last water date?
-            <input type="checkbox" />
-          </div>
-          <button onClick={addToCollection}> Add</button>
-        </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 export default SearchPagePlantModal;
