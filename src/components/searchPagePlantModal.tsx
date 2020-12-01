5f326b33554f2bed93363032d906ece26aaead07
tyannarae <tyannarae1994@gmail.com>
2020-11-30 18:00:05 -0600

    created drop down menu for user to use when adding a plant to collections

--- src/components/searchPagePlantModal.tsx
+++ src/components/searchPagePlantModal.tsx
@@ -1,50 +1,99 @@
 import React, { FunctionComponent } from "react";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 import { useSearchContext } from '../context/pages/searchPage'
+import { DirectionFacing } from "../models/directionFacing";
+
 import '../styles/components/searchPagePlantModal.scss'
 
 const SearchPagePlantModal: FunctionComponent = () => {
-    const {selectedPlant }= useSearchContext();
-    const {isModalOpen, setModalOpen} = useSearchContext();
-  const closeModal = () =>{
+const {selectedPlant }= useSearchContext();
+const {isModalOpen, setModalOpen} = useSearchContext();
+const closeModal = () =>{
     setModalOpen(!isModalOpen)
-  }
+}
+
+Object.keys(DirectionFacing).map((type) => (
+    console.log(type)
+    ));
+    
+const options = [ 
+    DirectionFacing.north,
+    DirectionFacing.northEast,
+    DirectionFacing.northWest,
+    DirectionFacing.west,
+    DirectionFacing.east,
+    DirectionFacing.south,
+    DirectionFacing.southEast
+  ];
 
   const getLightRequirements =() =>{
-      
       if (selectedPlant?.lightRequirements === undefined) {
          return undefined
-      } else {
-        
+      }
+       else {
           return(
             <div>Light Requirements: 
-                        {selectedPlant?.lightRequirements.map((lightReq) => (
+                {selectedPlant?.lightRequirements.map((lightReq) => (
                     <div>{lightReq}</div>
-          
-          ))}
+                ))}
             </div> 
           )        
         }
-  
   }
+
+
     return (
-       
-            <div className={'searchPageModalContainer'}>
-                <button className={'exitButton'}onClick={closeModal}> X </button>
-                <div>{selectedPlant?.commonName[0]}</div>
-                <img src={selectedPlant?.img} alt=''></img>
-                <div> Scientific Name: {selectedPlant?.scientificName}</div>
-                <div> Difficulty: {selectedPlant?.difficultyLevel}</div>
-                <div> Humidity Level: {selectedPlant?.humidityLevel}</div>
+      <div className={"searchPageModalContainer"}>
+        <button className={"exitButton"} onClick={closeModal}>
+          {" "}
+          X{" "}
+        </button>
+        <div>{selectedPlant?.commonName[0]}</div>
+        <img src={selectedPlant?.img} alt=""></img>
+        <div> Scientific Name: {selectedPlant?.scientificName}</div>
+        <div> Difficulty: {selectedPlant?.difficultyLevel}</div>
+        <div> Humidity Level: {selectedPlant?.humidityLevel}</div>
+        {getLightRequirements()}
+        <div>Min Temp: {selectedPlant?.minTemp}</div>
+        <div>Max Temp: {selectedPlant?.maxTemp}</div>
+        <div>Misting Requirement: {selectedPlant?.misting}</div>
+        <div className="AddToCollectionContainer">
+          Add {selectedPlant?.commonName[0]} to your collection:
+          <div>
+            what direction does this plant face?
+            <Dropdown
+              options={options}
+              placeholder="Select an option"
+            ></Dropdown>
+          </div>
+          <div>
+            {" "}
+            Is it located in a window seal?
+            <input type="checkbox" />
+          </div>
+          <div>
+            {" "}
+            Is it located under a grow light?
+            <input type="checkbox" />
+          </div>
+          <div>
+            {" "}
+            When was the last water date?
+            <input type="checkbox" />
+          </div>
+          <select
+            className="selectBox"
           
-                {getLightRequirements()}
-        
-                
-                <div>Min Temp: {selectedPlant?.minTemp}</div>
-                <div>Max Temp: {selectedPlant?.maxTemp}</div>
-                <div>Misting Requirement: {selectedPlant?.misting}</div>
-
-            </div>
-
-    )
+          >
+            {/* {Object.keys(DirectionFacing).map((type) => (
+              <option key={"buttonInspectorButtonSize" + type} value={type}>
+                {type}
+              </option>
+            ))} */}
+          </select>
+        </div>
+      </div>
+    );
 }
 export default SearchPagePlantModal;
