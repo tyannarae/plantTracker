c8471bc833fba9bb50f33f79c73a460bc6e45635
tyannarae <tyannarae1994@gmail.com>
2020-12-03 13:58:10 -0600

    trying to figure out the burger nav...

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -12,12 +12,38 @@ const NavBar = () => {
 	function handleSignOutClick() {
 		history.push("/");
 	}
+
+	function displayDropdownMenu() {
+		console.log("clicked shit");
+	}
+
 	return (
 		<nav className="navbar">
 			<div className="navbar-brand">
 				<a className="navbar-item" href="/search">
 					<img className="logoImg" alt="Logo" src={Logo} />
 				</a>
+				<div
+					role="button"
+					className="navbar-burger burger"
+					aria-label="menu"
+					aria-expanded="false"
+					data-target="navbarBasicExample"
+					onClick={displayDropdownMenu}
+				>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+
+					{/* <div className="navbar-dropdown">
+						<a href="/" className="navbar-item">
+							test
+						</a>
+						<a href="/" className="navbar-item">
+							test
+						</a>
+					</div> */}
+				</div>
 			</div>
 			<div className="navbar-menu">
 				<div className="navbar-end">
