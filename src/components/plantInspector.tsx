c53745bb02d632aa8d9603474d58a563726b1195
tyannarae <tyannarae1994@gmail.com>
2020-12-08 17:46:24 -0600

    just trying to figure out bulmas tile layout

--- src/components/plantInspector.tsx
+++ src/components/plantInspector.tsx
@@ -1,23 +1,30 @@
-import React, { FunctionComponent } from "react";
-import { useSearchContext } from "../context/pages/searchPage";
+import React, { FunctionComponent, useState } from "react";
 import Dropdown from "react-dropdown";
 import DatePicker from "react-datepicker";
+import { useSearchContext } from "../context/pages/searchPage";
 import { DirectionFacing } from "../models/directionFacing";
+import { UserPlant, Plant, collectionName } from "../database/plants";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-dropdown/style.css";
 import "../styles/components/plantInspector.scss";
 
 const PlantInspector: FunctionComponent = () => {
+	const [underGrowLight, setGrowLight] = useState<boolean>(false);
+	const [isInWindow, setInWindow] = useState<boolean>(false);
+	const [directionFacing, setDirectionFacing] = useState<string | undefined>(
+		undefined
+	);
+	const [dateWateredLast, setWaterDate] = useState<Date>(new Date());
 	const { selectedPlant } = useSearchContext();
-	const { setModalOpen } = useSearchContext();
+	const { isModalOpen, setModalOpen } = useSearchContext();
+	const closeModal = () => {
+		setModalOpen(!isModalOpen);
+	};
 
 	const directionOptions: Array<string> = [];
 	Object.keys(DirectionFacing).map((direction) =>
 		directionOptions.push(direction)
 	);
-	const onClickClose = () => {
-		setModalOpen(false);
-	};
 	const plantName = selectedPlant?.commonName[0];
 
 	const getLightRequirements = () => {
@@ -36,59 +43,86 @@ const PlantInspector: FunctionComponent = () => {
 	};
 
 	const addToCollection = () => {
-		console.log("");
+		const currentPlant = selectedPlant as Plant;
+		const newPlant: UserPlant = {
+			id: currentPlant.id,
+			directionFacing: directionFacing as DirectionFacing,
+			inWindowSeal: isInWindow,
+			growLight: underGrowLight,
+			lastWaterDate: dateWateredLast,
+		};
+		const dbString = window.sessionStorage.getItem(collectionName);
+		let db = [];
+		if (dbString !== null) {
+			db = JSON.parse(dbString);
+		}
+		db.push(newPlant);
+		window.sessionStorage.setItem(collectionName, JSON.stringify(db));
 	};
+
 	return (
-		<div className={"searchPageModalContainer"}>
-			<button className={"exitButton"} onClick={onClickClose}>
-				X
-			</button>
-			<div className="itemNameAndImgContainer">
-				<img
-					className="searchPageModalImg"
-					src={selectedPlant?.img}
-					alt=""
-				></img>
-				<div>{plantName}</div>
-			</div>
-			<div className="itemAddToCollectionContainer">
-				Add {plantName} to your collection:
-				<div>
-					<input type="checkbox" />
-					Located in a Window Seal?
-				</div>
-				<div>
-					<input type="checkbox" />
-					Located under a Grow Light?
-				</div>
-				<div className="dateAndDropdownContainer">
-					<div>
-						<DatePicker
-							placeholderText={"Date Last Watered"}
-							onChange={(date) => console.log(date)}
-						></DatePicker>
-					</div>
-					<div className="dropdownContainer">
-						<Dropdown
-							className="directionFacingDropdown"
-							options={directionOptions}
-							placeholder="Pick Direction"
-						></Dropdown>
-					</div>
-				</div>
-				<button className="addButton" onClick={addToCollection}>
-					{" "}
-					Add
+		<div className={" tile is-ancestor searchPageModalContainer"}>
+			<div className="tile ">
+				<button
+					className={"tile is-child delete is-large exitButton"}
+					onClick={closeModal}
+				>
+					X
 				</button>
-			</div>
-			<div className="itemPlantDetailsContainer">
-				<div> Scientific Name: {selectedPlant?.scientificName}</div>
-				<div> Difficulty: {selectedPlant?.difficultyLevel}</div>
-				<div> Humidity Level: {selectedPlant?.humidityLevel}</div>
-				{getLightRequirements()}
-				<div>Min Temp: {selectedPlant?.minTemp}</div>
-				<div>Max Temp: {selectedPlant?.maxTemp}</div>
-				<div>Misting Requirement: {selectedPlant?.misting}</div>
+
+				<div className="tile is-6 is-child ">
+					<div className="tile is-child  itemNameAndImgContainer">
+						<figure className="image is-square">
+							<img src={selectedPlant?.img} alt=""></img>
+						</figure>
+						<div>{plantName}</div>
+					</div>
+				</div>
+				<div className="tile is-6 is-child ">
+					Add {plantName} to your collection:
+					<div>
+						<input
+							type="checkbox"
+							onChange={() => {
+								setInWindow(!isInWindow);
+							}}
+						/>
+						Located in a Window Seal?
+					</div>
+					<div>
+						<input
+							type="checkbox"
+							onChange={() => {
+								setGrowLight(!underGrowLight);
+							}}
+						/>
+						Located under a Grow Light?
+					</div>
+					<div className="dateAndDropdownContainer">
+						<div>
+							<DatePicker
+								placeholderText={"Date Last Watered"}
+								selected={dateWateredLast}
+								onChange={(date) => setWaterDate(date as Date)}
+							></DatePicker>
+						</div>
+
+						<div className=" dropdownContainer">
+							<Dropdown
+								onChange={(e) => {
+									setDirectionFacing(e.value);
+								}}
+								className="directionFacingDropdown"
+								options={directionOptions}
+								placeholder="Direction Facing"
+							></Dropdown>
+						</div>
+					</div>
+					<button className="addButton" onClick={addToCollection}>
+						{" "}
+						Add
+					</button>
+				</div>
 			</div>
 		</div>
 	);
