4720491945c870920836a29c9932b453a4dede9f
tyannarae <tyannarae1994@gmail.com>
2020-12-26 12:08:12 -0600

    Loading component render tests are done

---
+++ src/components/loading.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { Loading } from "./loading";
+
+describe("Loading", () => {
+	it("loading component renders with text", () => {
+		const { getByText } = render(<Loading />);
+		expect(getByText("loading...")).toBeDefined();
+	});
+});
